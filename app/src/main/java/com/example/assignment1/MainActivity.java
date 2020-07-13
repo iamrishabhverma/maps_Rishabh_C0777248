efacfc5718cd74555d04028ef78338c027a2cd45
iamrishabhverma <46955354+iamrishabhverma@users.noreply.github.com>
2020-07-13 12:29:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,198 @@
+package com.example.assignment1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private static final int REQUEST_CODE = 1;
+    private Marker homeMarker;
+    private Marker destMarker;
+
+    Polyline line;
+    Polygon shape;
+    private static final int POLYGON_SIDES = 4;
+    List<Marker> markers = new ArrayList();
+
+    // location with location manager and listener
+    LocationManager locationManager;
+    LocationListener locationListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                setHomeMarker(location);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (!hasLocationPermission())
+            requestLocationPermission();
+        else
+            startUpdateLocation();
+
+
+        // apply long press gesture
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+//                Location location = new Location("Your Destination");
+//                location.setLatitude(latLng.latitude);
+//                location.setLongitude(latLng.longitude);
+                // set marker
+                setMarker(latLng);
+            }
+
+            private void setMarker(LatLng latLng) {
+                MarkerOptions options = new MarkerOptions().position(latLng)
+                        .title("Your destination");
+
+                /*if (destMarker != null) clearMap();
+
+                destMarker = mMap.addMarker(options);
+
+                drawLine();*/
+
+                // check if there are already the same number of markers, we clear the map.
+                if (markers.size() == POLYGON_SIDES)
+                    clearMap();
+
+                markers.add(mMap.addMarker(options));
+                if (markers.size() == POLYGON_SIDES)
+                    drawShape();
+            }
+
+            private void drawShape() {
+                PolygonOptions options = new PolygonOptions()
+                        .fillColor(0x33000000)
+                        .strokeColor(Color.RED)
+                        .strokeWidth(5);
+
+                for (int i=0; i<POLYGON_SIDES; i++) {
+                    options.add(markers.get(i).getPosition());
+                }
+
+                shape = mMap.addPolygon(options);
+
+            }
+
+            private void clearMap() {
+
+                /*if (destMarker != null) {
+                    destMarker.remove();
+                    destMarker = null;
+                }
+
+                line.remove();*/
+
+                for (Marker marker: markers)
+                    marker.remove();
+
+                markers.clear();
+                shape.remove();
+                shape = null;
+            }
+            /*
+            private void drawLine() {
+                PolylineOptions options = new PolylineOptions()
+                        .color(Color.BLACK)
+                        .width(10)
+                        .add(homeMarker.getPosition(), destMarker.getPosition());
+                line = mMap.addPolyline(options);
+            }*/
+        });
+    }
+
+    private void startUpdateLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+
+        /*Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        setHomeMarker(lastKnownLocation);*/
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    }
+
+    private boolean hasLocationPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void setHomeMarker(Location location) {
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions options = new MarkerOptions().position(userLocation)
+                .title("You are here")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .snippet("Your Location");
+        homeMarker = mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (REQUEST_CODE == requestCode) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+            }
+        }
+    }
+}
